cb36514177568e8427010f2d99683dc9a482479f
mwansa-reuben <73103644+mwansa-reuben@users.noreply.github.com>
2026-05-29 13:23:29 +0200

    Update HeroSlider.tsx

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -10,7 +10,7 @@ const slides = [
   { src: team2, position: "object-[center_70%]" },
   { src: team3, position: "object-[center_75%]" },
   { src: team4, position: "object-[center_65%]" },
-  { src: team5, position: "object-[center_66%]" },
+  { src: team5, position: "object-[center_90%]" },
 ];
 
 const HeroSlider = () => {
