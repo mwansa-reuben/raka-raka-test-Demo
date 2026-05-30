690ef6b5873c458c531aa9e9bf687b3679c98933
mwansa-reuben <73103644+mwansa-reuben@users.noreply.github.com>
2026-05-30 22:29:32 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,8 +37,8 @@ const Header = () => {
                   link.isGallery
                     ? `border-2 rounded-md px-4 py-2 text-sm font-medium transition-colors ${
                         location.pathname === link.path
-                          ? "border-red-600 text-red-600"
-                          : "border-red-600 text-red-600 hover:border-gray-500 hover:text-gray-600"
+                          ? "text-pfimary"
+                          : "border-2 border-transparent text-muted-foreground hover:border-red-600 hover:text-red-600"
                       }`
                     : `nav-link-highlight text-sm font-medium transition-colors hover:text-primary ${
                         location.pathname === link.path
